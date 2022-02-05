9ffac597752d967c9f554a1dd978f1a64a08397b
ckdwlsrh <ckdwls6504@gmail.com>
2022-02-05 16:23:33 +0900

    delete

--- todo-react-app/src/AddTodo.js
+++ todo-react-app/src/AddTodo.js
@@ -18,12 +18,18 @@ class AddTodo extends React.Component {
         this.setState({ item: {title: ""}});
     }
 
+        
+    enterKeyEventHandler = (e) => {
+        if(e.key === 'Enter') {
+        this.onButtonClick();
+        }
+    }
     render() {
         return (
             <Paper style={{margin: 16, padding: 16}}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{paddingRight: 16}}>
-                        <TextField placeholder="Add todo here" fullWidth onChange={this.onInputChange} value={this.state.item.title} />
+                        <TextField placeholder="Add todo here" fullWidth onChange={this.onInputChange} value={this.state.item.title} onKeyPress={this.enterKeyEventHandler}/>
                     </Grid>
                     <Grid xs={1} md={1} item>
                         <Button fullWidth color="secondary" variant="outlined" onClick={this.onButtonClick}>
--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {ListItem, ListItemText, InputBase, Checkbox } from '@material-ui/core';
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
 
 class Todo extends React.Component {
     constructor(props) {
@@ -22,7 +23,13 @@ class Todo extends React.Component {
                         fullWidth={true}
                     />
                 </ListItemText>
+                <ListItemSecondaryAction>
+                    <IconButton aria-label='Delete Todo'>
+                        <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
+
         );
     }
 }
