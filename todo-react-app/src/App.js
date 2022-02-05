eeddbbf11c11351c9e8cd13fbae5b15b31f9a1f6
ckdwlsrh <ckdwls6504@gmail.com>
2022-02-05 15:25:00 +0900

    construct & pros

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
 import React from 'react';
 import Todo from './Todo';
 import './App.css';
 
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {id: "0", title: "hello world1", done: true},
+        {id: "1", title: "hello world1", done: false},
+      ],
+    };
+  }
   render() {
+    var todoItems = this.state.items.map((item, idx) =>(
+      <Todo item={item} key={idx} />
+    ));
     return (
       <div className="App">
-        <Todo />
-        <Todo />
-      </div>
+        {todoItems}
+        </div>
     );
   }
 
